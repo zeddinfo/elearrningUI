6c3b47bab3397225ff6b1be7931b035fa8049d11
adi_siswoyo <adisiswoyo202@gmail.com>
2022-04-14 21:26:00 +0700

    Config Setup Locator and Dio Service

---
+++ lib/application/app/nss_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class NssApp extends StatefulWidget {
+  NssApp({Key? key}) : super(key: key);
+
+  @override
+  State<NssApp> createState() => _NssAppState();
+}
+
+class _NssAppState extends State<NssApp> {
+  // final _navigationService = NavigationService();
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
